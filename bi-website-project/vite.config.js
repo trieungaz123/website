666bf2751a8b2139fc53d9b0b22b60b936fcb207
trieungaz123 <trieund@ecomobile.vm>
2024-06-03 10:42:19 +0700

    Initial commit

--- bi-website-project/vite.config.js
+++ bi-website-project/vite.config.js
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/website-n-t-t-nghi-p/', // Thay thế 'website-n-t-t-nghi-p' bằng tên repo của bạn
   plugins: [react()],
-})
+  build: {
+    outDir: 'build', // Đảm bảo rằng thư mục build là 'build'
+  },
+});
